303697ffae78ef73613fec5b54b41819f9b6e254
hojooneum <bldolphin96@gmail.com>
2022-05-19 23:46:18 +0900

    [Assignment]{assignment2.c} assignment2 source file added

---
+++ Assignments/assignment2/assignment2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+typedef struct
+{
+    int data; //node will store an integer
+    struct node* right_child; // right child
+    struct node* left_child; // left child
+} node ;
+
