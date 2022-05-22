a550fc3ccda59f3c9183b58d9b4f36c12824a384
hojooneum <bldolphin96@gmail.com>
2022-05-22 23:19:55 +0900

    [Assignment]{assignment2.c} assignment2.c is updated

--- Assignments/assignment2/assignment2.c
+++ Assignments/assignment2/assignment2.c
@@ -1,9 +1,33 @@
 #include <stdio.h>
+#define TRUE 1
+#define FALSE 0
 
 typedef struct
 {
     int data; //node will store an integer
     struct node* right_child; // right child
     struct node* left_child; // left child
-} node ;
+} node;
 
+typedef node** BinarySearchTree;
+
+BinarySearchTree Create();
+int IsEmpty(BinarySearchTree root);
+BinarySearchTree MakeBST(BinarySearchTree bst1, int item, BinarySearchTree bst2);
+BinarySearchTree Lchild(BinarySearchTree bst);
+BinarySearchTree Rchild(BinarySearchTree bst);
+BinarySearchTree Data(BinarySearchTree bst);
+void InorderTraversal(BinarySearchTree bst);
+void PreorderTraversal(BinarySearchTree bst);
+void PostorderTraversal(BinarySearchTree bst);
+void LevelorderTraversal(BinarySearchTree bst);
+
+int main() {
+    return 0;
+}
+
+void InorderTraversal(BinarySearchTree bst) {
+    if (*bst) {
+        
+    }
+}
